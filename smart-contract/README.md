d5b35b766e822223cde59ffb514c0e4f64769530
Hung Pham <phamhungbkhn@gmail.com>
2021-11-08 16:47:14 +0700

    [Feature] Create Exchange contract that can swap between ETH and token.

---
+++ smart-contract/README.md
@@ -0,0 +1,18 @@
+# Basic Sample Hardhat Project
+We create simple exchange smart contract.
+
+Ideal come from https://medium.com/coinmonks/programming-defi-uniswap-part-1-839ebe796c7b
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+```
---
+++ smart-contract/test/exchange.test.js
@@ -0,0 +1,81 @@
+require("@nomiclabs/hardhat-waffle");
+const { expect } = require("chai");
+
+const toWei = (value) => ethers.utils.parseEther(value.toString());
+
+const fromWei = (value) =>
+    ethers.utils.formatEther(
+        typeof value === "string" ? value : value.toString()
+    );
+
+const getBalance = ethers.provider.getBalance;
+
+describe("Exchange", () => {
+    beforeEach(async () => {
+        [owner, user] = await ethers.getSigners();
+
+        const Token = await ethers.getContractFactory("Token");
+        token = await Token.deploy("CoinX", "XXX", toWei(1000000));
+        await token.deployed();
+
+        const Exchange = await ethers.getContractFactory("Exchange");
+        exchange = await Exchange.deploy(token.address);
+        await exchange.deployed();
+    });
+
+    it("is deployed", async () => {
+        expect(await exchange.deployed()).to.equal(exchange);
+    });
+
+    describe("addLiquidity", async () => {
+        it("adds liquidity 100 tokens", async () => {
+            await token.approve(exchange.address, toWei(200));
+            await exchange.addLiquidity(toWei(200), { value: toWei(100) });
+
+            expect(await getBalance(exchange.address)).to.equal(toWei(100));
+            expect(await exchange.getReserve()).to.equal(toWei(200));
+        });
+
+        it("adds liquidity 200 tokens", async () => {
+            await token.approve(exchange.address, toWei(200));
+            await exchange.addLiquidity(toWei(200), { value: toWei(200) });
+
+            expect(await getBalance(exchange.address)).to.equal(toWei(200));
+            expect(await exchange.getReserve()).to.equal(toWei(200));
+        });
+
+    });
+
+    describe("getTokenAmount", async () => {
+        it("returns correct token amount", async () => {
+            await token.approve(exchange.address, toWei(2000));
+            await exchange.addLiquidity(toWei(2000), { value: toWei(1000) });
+
+            let tokensOut = await exchange.getTokenAmount(toWei(1));
+            expect(fromWei(tokensOut)).to.equal("1.998001998001998001");
+
+            tokensOut = await exchange.getTokenAmount(toWei(100));
+            expect(fromWei(tokensOut)).to.equal("181.818181818181818181");
+
+            tokensOut = await exchange.getTokenAmount(toWei(1000));
+            expect(fromWei(tokensOut)).to.equal("1000.0");
+        });
+    });
+
+    describe("getEthAmount", async () => {
+        it("returns correct ether amount", async () => {
+            await token.approve(exchange.address, toWei(2000));
+            await exchange.addLiquidity(toWei(2000), { value: toWei(1000) });
+
+            let ethOut = await exchange.getEthAmount(toWei(2));
+            expect(fromWei(ethOut)).to.equal("0.999000999000999");
+
+            ethOut = await exchange.getEthAmount(toWei(100));
+            expect(fromWei(ethOut)).to.equal("47.619047619047619047");
+
+            ethOut = await exchange.getEthAmount(toWei(2000));
+            expect(fromWei(ethOut)).to.equal("500.0");
+        });
+    });
+
+})
