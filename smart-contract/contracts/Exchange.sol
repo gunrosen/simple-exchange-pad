5f2b9c289b127831d8bb88912473f69b9edcae63
Hung Pham <phamhungbkhn@gmail.com>
2021-11-09 15:52:37 +0700

    [Feature] LP-token and fee

--- smart-contract/contracts/Exchange.sol
+++ smart-contract/contracts/Exchange.sol
@@ -1,20 +1,56 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Exchange {
+contract Exchange  is ERC20{
     address public tokenAddress;
 
-    constructor(address _token) {
+    constructor(address _token) ERC20("LP SWAP token", "LP-SWAP"){
         require(_token != address(0), "invalid token address");
 
         tokenAddress = _token;
     }
-    // @dev send _tokenAmount from sender to Exchange contract
-    function addLiquidity(uint256 _tokenAmount) public payable {
-        IERC20 token = IERC20(tokenAddress);
-        token.transferFrom(msg.sender, address(this), _tokenAmount);
+    // @dev the amount of LP-tokens issued equals to the amount of ethers deposited.
+    function addLiquidity(uint256 _tokenAmount) public payable  returns(uint256){
+        // If this is a new exchange (no liquidity), allow an arbitrary liquidity proportion when pool is empty.
+        if (getReserve() == 0) {
+            IERC20 token = IERC20(tokenAddress);
+            token.transferFrom(msg.sender, address(this), _tokenAmount);
+            uint256 liquidity = address(this).balance;
+            _mint(msg.sender, liquidity);
+
+            return liquidity;
+        }else {
+            // only add if keep current rate
+            // This will preserve a price when liquidity is added to a pool
+            uint256 ethReserve = address(this).balance - msg.value;
+            uint256 tokenReserve = getReserve();
+            uint256 tokenAmount = (msg.value * tokenReserve) / ethReserve;
+            require(_tokenAmount >= tokenAmount, "insufficient token amount");
+
+            IERC20 token = IERC20(tokenAddress);
+            token.transferFrom(msg.sender, address(this), tokenAmount);
+
+            uint256 liquidity = (totalSupply() * msg.value) / ethReserve;
+            _mint(msg.sender, liquidity);
+
+            return liquidity;
+        }
+    }
+    // @dev: _amount is liquidity
+    function removeLiquidity(uint256 _amount) public returns (uint256, uint256) {
+        require(_amount > 0, "invalid amount");
+
+        uint256 ethAmount = (address(this).balance * _amount) / totalSupply();
+        uint256 tokenAmount = (getReserve() * _amount) / totalSupply();
+
+        _burn(msg.sender, _amount);
+        payable(msg.sender).transfer(ethAmount);
+        IERC20(tokenAddress).transfer(msg.sender, tokenAmount);
+
+        return (ethAmount, tokenAmount);
     }
 
     // @dev get balanceOf of Exchange contract
@@ -22,8 +58,9 @@ contract Exchange {
         return IERC20(tokenAddress).balanceOf(address(this));
     }
 
-    // @dev
-    function getAmount(
+    // @dev keep: (x - dx)( y + dy) = xy
+    //
+    function getAmount_old(
         uint256 inputAmount,
         uint256 inputReserve,
         uint256 outputReserve
@@ -32,7 +69,24 @@ contract Exchange {
 
         return (inputAmount * outputReserve) / (inputReserve + inputAmount);
     }
-    // @dev get amount of token when buy ETH
+
+    // @dev apply fee as 1%
+    // Fees is calculated by subtracting directly when they sent to exchange contract
+    function getAmount(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+    ) private pure returns (uint256) {
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+
+        uint256 inputAmountWithFee = inputAmount * 99;
+        uint256 numerator = inputAmountWithFee * outputReserve;
+        uint256 denominator = (inputReserve * 100) + inputAmountWithFee;
+
+        return numerator / denominator;
+    }
+
+    // @dev get amount of token when sell ETH
     function getTokenAmount(uint256 _ethSold) public view returns (uint256) {
         require(_ethSold > 0, "ethSold is too small");
 
@@ -41,7 +95,7 @@ contract Exchange {
         return getAmount(_ethSold, address(this).balance, tokenReserve);
     }
 
-    // @dev get eth when buy token
+    // @dev get eth when sell token
     function getEthAmount(uint256 _tokenSold) public view returns (uint256) {
         require(_tokenSold > 0, "tokenSold is too small");
 
@@ -50,7 +104,7 @@ contract Exchange {
         return getAmount(_tokenSold, tokenReserve, address(this).balance);
     }
 
-    // @dev swap from ETH to token
+    // @dev swap from ETH to token, amount of token must greater then _mintokens
     function ethToTokenSwap(uint256 _minTokens) public payable {
         uint256 tokenReserve = getReserve();
         uint256 tokensBought = getAmount(
@@ -64,7 +118,7 @@ contract Exchange {
         IERC20(tokenAddress).transfer(msg.sender, tokensBought);
     }
 
-    // @dev swap token to ETH
+    // @dev swap token to ETH, bought ETH must be greater than _minEth
     function tokenToEthSwap(uint256 _tokensSold, uint256 _minEth) public {
         uint256 tokenReserve = getReserve();
         uint256 ethBought = getAmount(
