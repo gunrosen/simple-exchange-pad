c87d7bbab1136d4ea1991b4a0a11bbf4bc8627d0
Hung Pham <phamhungbkhn@gmail.com>
2021-11-12 13:55:34 +0700

    [Config] hardhat points build artifacts to frontend src

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -18,6 +18,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  paths: {
+    artifacts: '../../frontend/src/artifacts',
+  },
   networks: {
     hardhat: {
       chainId: 1337,
